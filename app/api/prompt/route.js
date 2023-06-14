da0d2aa089f1250850d50092f8a2130b163aa20a
Shahin Mahmud <sm.polash@gmail.com>
2023-06-14 19:18:42 +0600

    Post List page done

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -7,7 +7,6 @@ export const GET = async (request) => {
 
     const prompts = await Prompt.find({}).populate("creator");
     return new Response(JSON.stringify(prompts), { status: 200 });
-
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
